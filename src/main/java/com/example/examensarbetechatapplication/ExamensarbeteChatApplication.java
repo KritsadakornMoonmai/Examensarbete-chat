1ebddf0aa75cf77e8ad2b654bc1cb2d2ed142382
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2025-04-15 15:13:52 +0200

    Start project

---
+++ src/main/java/com/example/examensarbetechatapplication/ExamensarbeteChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.examensarbetechatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensarbeteChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamensarbeteChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examensarbetechatapplication/ExamensarbeteChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examensarbetechatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamensarbeteChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
